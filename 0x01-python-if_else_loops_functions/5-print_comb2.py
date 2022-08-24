61aca315a40a9765b674a95996935d7c355cc696
Eninetjanice <enijaneth@gmail.com>
2022-08-24 00:14:06 +0000

    Print numbers from 0-99 in 2 combo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    if i < 99:
+for i in range(0, 100):
+    if i < 100:
         print("{0:02d}".format(i), end=", ")
     else:
         print("{:d}".format(99))
