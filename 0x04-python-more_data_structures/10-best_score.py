03d18ac367e7649ae992b791396c1ad6be1acaf3
Eninetjanice <71446962+Eninetjanice@users.noreply.github.com>
2022-09-01 01:03:24 +0100

    Func that returns a key with the biggest int value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dict):
     if a_dict is None or a_dict == {}:
         return None
-    maxval = max(my_dict.values())
-    for key, value in my_dict.items():
-        if value is biggest:
+    maxval = max(a_dict.values())
+    for key, value in a_dict.items():
+        if value is maxval:
             return key
