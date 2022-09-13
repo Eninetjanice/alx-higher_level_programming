f3bed179daee2b5af8fa403886be341e52d5c5ff
Eninetjanice <enijaneth@gmail.com>
2022-09-13 10:57:24 +0000

    Added exception for IndexError in file 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[index]), end='')
             i += 1
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             pass
+        except (IndexError):
+            i += 1
         index += 1
     print()
     return i
