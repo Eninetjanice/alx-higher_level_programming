c9bf8722808fa0b78433d8e7697b3543e555194d
Eninetjanice <enijaneth@gmail.com>
2022-09-14 03:03:21 +0000

    Advance tasks

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "python.h"
+/**
+ * print_python_list - print some basic info about Python lists
+ * @p: python object
+ * Return: nothing
+ **/
+void print_python_list(PyObject *p)
+{
+}
+
+/**
+ * print_python_bytes - print some basic info about Python bytes objects
+ * @p: python object
+ * Return: nothing
+ **/
+void print_python_bytes(PyObject *p)
+{
+}
+
+/**
+ * print_python_float - print basic info about Python float
+ * @p: python float
+ * Return: nothing
+ **/
+void print_python_float(PyObject *p)
+{
+}
---
+++ 0x05-python-exceptions/python.h
@@ -0,0 +1,8 @@
+#define PYTHON_H
+#ifndef PYTHON_H
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif
