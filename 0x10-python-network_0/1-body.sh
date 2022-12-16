47581abe408e3900b85db83216f157d265ce32b6
Eninetjanice <71446962+Eninetjanice@users.noreply.github.com>
2022-12-16 06:26:57 +0100

    Fixing length criteria

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends GET and displays body of response
-# Displays only body of a 200 status code response using curl
+# This script takes in a URL, sends GET & displays 200 status code response body
 curl -sLX GET "$1"
