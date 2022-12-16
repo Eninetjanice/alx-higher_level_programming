77c598a8a2ce39dc2692c38fd3a092cd60f473a3
Eninetjanice <71446962+Eninetjanice@users.noreply.github.com>
2022-12-16 06:20:20 +0100

    Wc -l compliant

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL
-#  & displays the size of the body of the response in bytes using curl
+# Script takes in a URL, sends request 2 it & displays body size in bytes
 curl "$1" | wc -c
