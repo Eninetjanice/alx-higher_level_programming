34990a98972fe320d32735b46c2f225e75f500ab
Janeth Eni <enijaneth@gmail.com>
2023-02-05 04:37:29 +0000

    Module fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
-
+"""
+script that takes in arguments and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+Using MySQL injections!
+"""
 import MySQLdb
 from sys import argv
 
-'''
-script that lists all states from the database
-'''
+
 if __name__ == "__main__":
     cont = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
